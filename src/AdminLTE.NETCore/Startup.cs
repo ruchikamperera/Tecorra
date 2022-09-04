85afa76c306fcb4d749ac9878cbc795d39826746
ruchikamperera <ruchikamperera@outlook.com>
2022-09-04 22:18:44 +0530

    changes

---
+++ src/AdminLTE.NETCore/Startup.cs
@@ -0,0 +1,74 @@
+﻿using AdminLTE.NETCore.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO.Compression;
+
+namespace AdminLTE.NETCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //Add Memory Cache
+            //https://docs.microsoft.com/en-us/aspnet/core/performance/caching/memory
+            services.AddMemoryCache();
+
+            services.Configure<GzipCompressionProviderOptions>
+                (options => options.Level = CompressionLevel.Fastest);
+            services.AddResponseCompression(options =>
+            {
+                options.Providers.Add<GzipCompressionProvider>();
+            });
+            
+            //Add the ControllerInformationRepository
+            services.AddSingleton<IControllerInformationRepository, ControllerInformationRepository>();
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {            
+            app.UseStaticFiles();
+
+            app.UseResponseCompression();
+                        
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Dashboard}/{action=Dashboardv1}/{id?}");
+            });
+        }
+    }
+}
